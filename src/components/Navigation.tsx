9e0d474c81a124e59d428fcd43027bfd4a1fc0c1
Kishan Raja <kraja@experientgroup.com>
2023-02-17 15:18:35 -0500

    testing push

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Navigation() {
+  return (
+    <div className="App">
+      <nav>
+        <a>Home</a>
+        <a>Weather</a>
+      </nav>
+    </div>
+  );
+}
+
+export default Navigation;
